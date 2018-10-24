3e47a97d935f12fd053436b8b6ef333f0c8997ef
ebarra <ebarra@dit.upm.es>
2018-10-24 15:49:37 +0200

    server v0

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+const crypto = require('crypto');
+const secret = 'ab';
+const code_length = 2; //number of characters that this app manages
+
+const app = express();
+
+//Middlewares
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+//ROUTES:
+app.post('/code', function(req, res, next) {
+  const hash = crypto.createHmac('sha256', secret)
+                   .update(req.body.key)
+                   .digest('hex');
+  console.log(hash);
+  res.json({ status: "ok", code: hash.slice(-code_length) });
+});
+
+app.post('/check', function(req, res, next) {
+  var key = req.body.key;
+  var code = req.body.code;
+
+  const hash = crypto.createHmac('sha256', secret)
+                   .update(req.body.key)
+                   .digest('hex');
+                   console.log(hash);
+  const rightcode = hash.slice(-code_length);
+  if(code===rightcode){
+    res.json({ status: "ok", message: 'Bien!' });
+  } else {
+    res.json({ status: "error", message: 'Mal!' });
+  }
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  console.log(err);
+  res.send('error');
+});
+
+app.listen(3000, () => console.log('Server app listening on port 3000!'));
---
+++ public/js/script.js
@@ -0,0 +1,51 @@
+
+/*
+ fetch('http://example.com/movies.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
+*/
+
+
+
+  window.addEventListener("load", function () {
+    function sendData(url) {
+      var data = {};
+      data.key = document.getElementById("mykey").value;
+      data.code  = document.getElementById("mycode").value;
+      var json = JSON.stringify(data);
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+      xhr.onload = function () {
+      	var res = JSON.parse(xhr.responseText);
+      	if (xhr.readyState === 4 && xhr.status === 200) {
+      		console.log(res);
+          if(res.status==="ok"){
+            document.getElementById("mycontent").style.display = 'none';
+            document.getElementById("myprize").style.display = 'block';
+          } else {
+            document.getElementById("retry").style.display = 'block';
+          }
+      	} else {
+      		console.log(res);
+      	}
+      }
+      xhr.send(json);
+    }
+
+
+    document.getElementById("myform").addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      sendData("/check");
+    });
+
+    document.getElementById("mytest").addEventListener("click", function (event) {
+      sendData("/code");
+    });
+});
