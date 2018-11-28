fb748ad66b98a5914b3d3b6f5b4ee5aba229c09a
aemartos <aemartos@gmail.com>
2018-11-28 12:09:10 +0100

    Minor changes

--- app.js
+++ app.js
@@ -13,43 +13,54 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // support encoded bodies
 
 //ROUTES:
-app.post('/code', function(req, res, next) {
+app.post('/code', function (req, res, next) {
   const hash = crypto.createHmac('sha256', secret)
-                   .update(req.body.key)
-                   .digest('hex');
+    .update(req.body.key)
+    .digest('hex');
   console.log(hash);
-  res.json({ status: "ok", code: hash.slice(-code_length) });
+  res.json({
+    status: "ok",
+    code: hash.slice(-code_length)
+  });
 });
 
-app.post('/check', function(req, res, next) {
+app.post('/check', function (req, res, next) {
   var key = req.body.key;
   var code = req.body.code;
 
   const hash = crypto.createHmac('sha256', secret)
-                   .update(req.body.key)
-                   .digest('hex');
-                   console.log(hash);
+    .update(req.body.key)
+    .digest('hex');
+  console.log(hash);
   const rightcode = hash.slice(-code_length);
-  if(code===rightcode){
-    res.json({ status: "ok", message: 'Bien!' });
+  if (code === rightcode) {
+    res.json({
+      status: "ok",
+      message: 'Bien!'
+    });
   } else {
-    res.json({ status: "error", message: 'Mal!' });
+    res.json({
+      status: "error",
+      message: 'Mal!'
+    });
   }
 });
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -60,4 +71,4 @@ app.use(function(err, req, res, next) {
   res.send('error');
 });
 
-app.listen(3000, () => console.log('Server app listening on port 3000!'));
+app.listen(3001, () => console.log('Server app listening on port 3001!'));
