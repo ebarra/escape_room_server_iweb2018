6b4546db6138e8eb58f1b82480c212d0341d481d
aemartos <aemartos@gmail.com>
2018-11-29 12:51:01 +0100

    Change file structure

--- app.js
+++ app.js
@@ -1,74 +1,48 @@
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+dotenv.config({path: path.join(__dirname, '.private.env')});
+dotenv.config({path: path.join(__dirname, '.public.env')});
+
+const bodyParser = require('body-parser');
 const express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var bodyParser = require('body-parser');
-const crypto = require('crypto');
-const secret = 'ab';
-const code_length = 2; //number of characters that this app manages
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const logger = require('morgan');
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
 
 //Middlewares
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(logger('dev'));
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({
-  extended: true
-})); // support encoded bodies
-
-//ROUTES:
-app.post('/code', function (req, res, next) {
-  const hash = crypto.createHmac('sha256', secret)
-    .update(req.body.key)
-    .digest('hex');
-  console.log(hash);
-  res.json({
-    status: "ok",
-    code: hash.slice(-code_length)
-  });
-});
-
-app.post('/check', function (req, res, next) {
-  var key = req.body.key;
-  var code = req.body.code;
-
-  const hash = crypto.createHmac('sha256', secret)
-    .update(req.body.key)
-    .digest('hex');
-  console.log(hash);
-  const rightcode = hash.slice(-code_length);
-  if (code === rightcode) {
-    res.json({
-      status: "ok",
-      message: 'Bien!'
-    });
-  } else {
-    res.json({
-      status: "error",
-      message: 'Mal!'
-    });
-  }
-});
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public/stylesheets/sass'),
+  dest: path.join(__dirname, 'public/stylesheets'),
+  indentedSyntax: true,
+  outputStyle: 'extended', //'compressed'
+  sourceMap: true
+}));
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  console.log(err);
-  res.send('error');
-});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-app.listen(3001, () => console.log('Server app listening on port 3001!'));
+
+// default value for title local
+app.locals.title = 'The Bomb!';
+
+
+// routes
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const crypto = require('crypto');
+const secret = 'ab';
+const code_length = 2; //number of characters that this app manages
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.post('/code', function (req, res, next) {
+  const hash = crypto.createHmac('sha256', secret)
+    .update(req.body.key)
+    .digest('hex');
+  console.log(hash);
+  res.json({
+    status: "ok",
+    code: hash.slice(-code_length)
+  });
+});
+
+router.post('/check', function (req, res, next) {
+  var key = req.body.key;
+  var code = req.body.code;
+
+  const hash = crypto.createHmac('sha256', secret)
+    .update(req.body.key)
+    .digest('hex');
+  console.log(hash);
+  const rightcode = hash.slice(-code_length);
+  if (code === rightcode) {
+    res.json({
+      status: "ok",
+      message: 'Bien!'
+    });
+  } else {
+    res.json({
+      status: "error",
+      message: 'Mal!'
+    });
+  }
+});
+
+
+module.exports = router;
+
+
+//ROUTES:
+// app.post('/code', function (req, res, next) {
+//   const hash = crypto.createHmac('sha256', secret)
+//     .update(req.body.key)
+//     .digest('hex');
+//   console.log(hash);
+//   res.json({
+//     status: "ok",
+//     code: hash.slice(-code_length)
+//   });
+// });
+
+// app.post('/check', function (req, res, next) {
+//   var key = req.body.key;
+//   var code = req.body.code;
+
+//   const hash = crypto.createHmac('sha256', secret)
+//     .update(req.body.key)
+//     .digest('hex');
+//   console.log(hash);
+//   const rightcode = hash.slice(-code_length);
+//   if (code === rightcode) {
+//     res.json({
+//       status: "ok",
+//       message: 'Bien!'
+//     });
+//   } else {
+//     res.json({
+//       status: "error",
+//       message: 'Mal!'
+//     });
+//   }
+// });
